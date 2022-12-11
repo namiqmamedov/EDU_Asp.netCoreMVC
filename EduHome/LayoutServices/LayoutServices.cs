429afc2c7afbbce0dc940ad6ee01846caff8f97c
namiqmamedov <namigmm@code.edu.az>
2022-12-11 04:46:54 +0400

    feat:Create DB & Views

---
+++ EduHome/LayoutServices/LayoutServices.cs
@@ -0,0 +1,19 @@
+﻿using EduHome.DAL;
+using EduHome.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EduHome.LayoutServices
+{
+    public class LayoutServices : ILayoutServices
+    {
+        private readonly AppDbContext _context;
+        public LayoutServices(AppDbContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
