cb6943953fb48303d448377ee715f67ee6cdb1c4
namiqmamedov <namigmm@code.edu.az>
2022-12-17 02:58:21 +0400

    refactor: updating the teachers of the area

---
+++ EduHome/Areas/Manage/Controllers/TeacherController.cs
@@ -0,0 +1,35 @@
+﻿using EduHome.DAL;
+using EduHome.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EduHome.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class TeacherController : Controller
+    {
+        private readonly AppDbContext _context;
+        public TeacherController(AppDbContext context)
+        {
+            _context = context;
+        }
+      
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Teacher> teachers = await _context.Teachers.
+                Where(t => t.IsDeleted == false).ToListAsync();
+
+            return View(teachers);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+    }
+}
