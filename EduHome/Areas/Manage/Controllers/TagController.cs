a134c16d90992dfb07dcb24a5c93af4f90493cbd
namiqmamedov <namigmm@code.edu.az>
2022-12-17 01:15:58 +0400

    refactor: Update Areas

---
+++ EduHome/Areas/Manage/Controllers/TagController.cs
@@ -0,0 +1,24 @@
+﻿using EduHome.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EduHome.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class TagController : Controller
+    {
+        private readonly AppDbContext _context;
+        public TagController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Tags.Where(t=>t.IsDeleted == false).ToListAsync());
+        }
+    }
+}
